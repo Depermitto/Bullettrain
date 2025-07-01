5315333d06c07bcc686ca55b5d4e50421bd17b8b
Depermitto <penciller@disroot.org>
2025-07-01 22:48:07 +0200

    persistent program creation

--- app/src/main/java/org/depermitto/App.kt
+++ app/src/main/java/org/depermitto/App.kt
@@ -3,10 +3,12 @@ package org.depermitto
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import org.depermitto.database.GymDatabase
+import org.depermitto.presentation.ProgramCreationViewModel
 import org.depermitto.ui.Ribbon
 import org.depermitto.ui.Scaffold
 import org.depermitto.ui.screens.*
@@ -19,7 +21,9 @@ fun App(db: GymDatabase, dbFile: File, fallbackBytes: ByteArray) = MaterialTheme
     val scope = rememberCoroutineScope()
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = Screen.ProgramsCreationScreen.route) {
+    val programCreationViewModel: ProgramCreationViewModel = viewModel()
+
+    NavHost(navController = navController, startDestination = Screen.MainScreen.route) {
         composable(Screen.MainScreen.route) {
             Scaffold(ribbon = { Ribbon(navController = navController, backButton = false) }) {
                 MainScreen(db.getProgramDao(), navController)
@@ -28,7 +32,11 @@ fun App(db: GymDatabase, dbFile: File, fallbackBytes: ByteArray) = MaterialTheme
 
         composable(Screen.ProgramsCreationScreen.route) {
             Scaffold(ribbon = { Ribbon(navController = navController) }) {
-                ProgramsCreationScreen(programDao = db.getProgramDao(), navController = navController)
+                ProgramsCreationScreen(
+                    programCreationViewModel,
+                    programDao = db.getProgramDao(),
+                    navController = navController
+                )
             }
         }
 
--- app/src/main/java/org/depermitto/ui/screens/ProgramsCreationScreen.kt
+++ app/src/main/java/org/depermitto/ui/screens/ProgramsCreationScreen.kt
@@ -1,61 +1,87 @@
 package org.depermitto.ui.screens
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.depermitto.database.Day
+import org.depermitto.database.Program
 import org.depermitto.database.ProgramDao
+import org.depermitto.presentation.ProgramCreationViewModel
 import org.depermitto.ui.DayCreation
 
-// TODO Biggest road blocker as of this moment,
-//  can't create any plans and the ui is clunky
 @Composable
-fun ProgramsCreationScreen(programDao: ProgramDao, navController: NavController) = Column(
-    modifier = Modifier
-        .fillMaxSize()
-        .padding(16.dp), horizontalAlignment = Alignment.CenterHorizontally
+fun ProgramsCreationScreen(
+    viewModel: ProgramCreationViewModel,
+    programDao: ProgramDao,
+    navController: NavController,
 ) {
-    var workoutName by remember { mutableStateOf("") }
-    TextField(
-        modifier = Modifier.fillMaxWidth(),
-        value = workoutName,
-        onValueChange = { workoutName = it },
-        placeholder = { Text(text = "Workout Name") },
-        shape = RoundedCornerShape(32.dp),
-        colors = TextFieldDefaults.colors(
-            focusedIndicatorColor = Color.Transparent,
-            unfocusedIndicatorColor = Color.Transparent,
-            disabledIndicatorColor = Color.Transparent
-        ),
-    )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 4.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = viewModel.state.workoutName,
+            onValueChange = { viewModel.setWorkoutName(it) },
+            placeholder = { Text(text = "Workout Name") },
+            shape = RoundedCornerShape(32.dp),
+            colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent
+            ),
+        )
 
-    val days = remember { mutableStateListOf<Day>() }
-    LazyColumn {
-        itemsIndexed(days) { i, day ->
-            DayCreation(
-                day = day,
-                onDayChanged = { if (it != null) days[i] = it else days.removeAt(i) },
-                navController = navController
-            )
+        LazyColumn(modifier = Modifier.weight(1f), horizontalAlignment = Alignment.CenterHorizontally) {
+            items(viewModel.state.days) { day ->
+                DayCreation(
+                    day = day, onDayChanged = {
+                        if (it != null) viewModel.set(day, it)
+                        else viewModel.removeDay(day)
+                    }, navController = navController
+                )
+            }
+            item {
+                Button(
+                    onClick = { viewModel.addDay(Day("Day ${viewModel.state.days.size + 1}")) },
+                    enabled = viewModel.state.days.size < 7,
+                ) {
+                    Text("Add Day")
+                }
+            }
+        }
+
+        val context = LocalContext.current
+        Button(onClick = {
+            val program = Program(name = viewModel.state.workoutName, trainingWork = viewModel.state.days)
+            CoroutineScope(Dispatchers.IO).launch {
+                programDao.upsert(program)
+            }
+            navController.popBackStack(Screen.MainScreen.route, false)
+            Toast.makeText(context, "Successfully Created", Toast.LENGTH_SHORT).show()
+        }) {
+            Text(text = "Complete Program")
         }
     }
-
-    Button(modifier = Modifier.padding(top = 8.dp), onClick = { days.add(Day("Day ${days.size + 1}")) }) {
-        Text("Add Day")
-    }
 }
-
